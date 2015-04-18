578b08118e3ebcccd9d8c1ed3a77902c98ec35e0
Klemen Brajkovič <d3cr1pt0r@gmail.com>
2015-04-19 01:34:55 +0200

    Added simple texture for gates and created basic camera and chip interactions

---
+++ Assets/Code/Klemen/BaseGateController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+enum PinType {
+	Input = 0,
+	Output = 1
+};
+
+public class BaseGateController : MonoBehaviour {
+
+	private Dictionary<string, float> inputs;
+	private Dictionary<string, float> outputs;
+
+	void Start () {
+		Time.fixedDeltaTime = 0.001f;
+	}
+
+	void addInput(string inputName) {
+		inputs.Add (inputName, 0);
+	}
+
+	void addOutput(string outputName) {
+		outputs.Add (outputName, 0);
+	}
+}
---
+++ Assets/Code/Klemen/GateGlowController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GateGlowController : MonoBehaviour {
+
+	public GameObject GlowGO;
+	public AnimationCurve GlowScaleAC;
+	public float animationSpeed;
+
+	private float animationTime;
+	private float lastMouseEnterTime;
+	private bool mouseEntered;
+
+	void Start () {
+		animationTime = 1.0f;
+		mouseEntered = false;
+	}
+
+	void Update () {
+		if (Time.time - lastMouseEnterTime > 0.05f) {
+			mouseEntered = false;
+			GlowGO.transform.localScale = Vector3.one;
+		}
+		if (animationTime < 1.0f) {
+			float newScale = GlowScaleAC.Evaluate(animationTime);
+			GlowGO.transform.localScale = new Vector3(newScale, newScale, newScale);
+			animationTime += Time.deltaTime * animationSpeed;
+		}
+	}
+
+	void OnMouseOver() {
+		lastMouseEnterTime = Time.time;
+		if (!mouseEntered)
+			animationTime = 0;
+		mouseEntered = true;
+	}
+}
