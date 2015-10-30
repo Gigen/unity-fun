999340c02b7ffe2621a875fe2e588016b2ab4351
Gregor Mubi <mubyg9@gmail.com>
2015-10-30 22:51:50 +0100

    Some preparation data

---
+++ Assets/Code/Gregor/CubeController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+    private float Angle = 0.0f;
+    private const float Speed = 0.05f;
+    private const float AngularSpeed = 1f;
+
+    void Update() {
+        if (Input.GetKey(KeyCode.W)) {
+            transform.position = transform.position + new Vector3(Mathf.Sin(Angle * Mathf.Deg2Rad), 0, Mathf.Cos(Angle * Mathf.Deg2Rad)) * Speed;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            Angle -= AngularSpeed;
+            transform.eulerAngles = new Vector3(0, Angle, 0);
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            transform.position = transform.position - new Vector3(Mathf.Sin(Angle * Mathf.Deg2Rad), 0, Mathf.Cos(Angle * Mathf.Deg2Rad)) * Speed;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            Angle += AngularSpeed;
+            transform.eulerAngles = new Vector3(0, Angle, 0);
+        }
+
+    }
+}
---
+++ Assets/Code/Gregor/MainCameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class MainCameraController : MonoBehaviour {
+
+    public Transform FollowingTransform = null;
+
+    private Vector3 OffsetVector = new Vector3(5, 5, -5);
+
+    void Start() {
+	
+    }
+
+    void Update() {
+        if (FollowingTransform != null) {
+            transform.position = FollowingTransform.position + OffsetVector;
+        }
+    }
+}
